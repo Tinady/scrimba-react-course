11c76bff067b39839e9e8def04c76bf8461478c0
Tina Dessu <tinady2009@gmail.com>
2023-03-28 23:07:38 +0300

    Update index.js

--- funfactsabtreact/src/index.js
+++ funfactsabtreact/src/index.js
@@ -1,23 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import  ReactDOM from "react-dom";
 
-const navBar=(
-  <nav>
-    <h1>Tina's List</h1>
-    <li>hi</li>
-    <li>hey</li>
-    <li>hello</li>
-  </nav>
+const page=(
+  <div>
+  <img src="./react-logo.png" width="480px"/>
+  <h1>Fun facts about react</h1>
+  <ul>
+    <li>was first released in 2013</li>
+    <li>was orginally created by Jordan Walke</li>
+    <li>has well over 100k starts on Github</li>
+  </ul>
+
+  </div>
 )
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  navBar
-  
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
+ReactDOM.render(page, document.getElementById("root"))
