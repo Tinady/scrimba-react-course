eba04e4374175f17f2d9c388e2071095f12871ce
Tina Dessu <tinady2009@gmail.com>
2023-03-28 22:37:35 +0300

    Update index.js

--- funfactsabtreact/src/index.js
+++ funfactsabtreact/src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const navBar=(
+  <nav>
+    <h1>Tina's List</h1>
+    <li>hi</li>
+    <li>hey</li>
+    <li>hello</li>
+  </nav>
+)
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  navBar
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
