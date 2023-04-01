6a19a7c51e85aaa6da09805525b41109864211eb
Tina Dessu <tinady2009@gmail.com>
2023-04-01 12:25:17 +0300

    Create Header.js

---
+++ funfactsabtreact/src/Header.js
@@ -0,0 +1,18 @@
+import logo from "./react-logo.png";
+import React from "react";
+
+function Header() {
+    return(
+    <header>
+      <nav className="nav">
+        <img src={logo} alt="" />
+        <ul className="nav-items">
+          <li>Pricing</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>)
+  }
+
+  export default Header;
