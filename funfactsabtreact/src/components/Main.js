6c6c812b24d086577e80611a7b093f5afb9db08c
Tina Dessu <tinady2009@gmail.com>
2023-04-01 21:15:11 +0300

    Update Main.js

--- funfactsabtreact/src/components/Main.js
+++ funfactsabtreact/src/components/Main.js
@@ -1,7 +1,18 @@
 
 import React from "react";
 function  MainContent(){
- <h1>NavBar goes here</h1>
+    return(
+   <div>
+    <h1> Fun Facts About React</h1>
+    <ul>
+        <li>Was first released in 2013</li>
+        <li>Was orginally created by Jordan Walke</li>
+        <li>Has well over 100k starts on Github</li>
+        <li> is mainatained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ul>
+   </div>
+        )
 
 }
 
