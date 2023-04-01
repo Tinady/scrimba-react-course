6b79ca49feab7c92456a6605f67af72808bc6824
Tina Dessu <tinady2009@gmail.com>
2023-04-01 22:04:20 +0300

    Navbar styling

--- funfactsabtreact/src/components/Navbar.js
+++ funfactsabtreact/src/components/Navbar.js
@@ -1,9 +1,14 @@
 import React from "react";
+import logo from "../imgs/react-logo.png"
+
 function  Navbar(){
     return( 
-        <div>
-            
-        </div>
+       <nav>
+       <img src={logo} className="nav--icon" />
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
+      
+      </nav>
     )
 
    
