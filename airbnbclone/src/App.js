784a8398fb3c27be86d296bef081e48d381cde62
Tina Dessu <tinady2009@gmail.com>
2023-04-02 19:40:02 +0300

    Update App.js

--- airbnbclone/src/App.js
+++ airbnbclone/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./components/nav"
+import Hero from "./components/Hero"
 import "./App.css"
 
 export default function App(){
    return(
     <div>
         <Navbar/>
+        <Hero/>
     </div>
 
    )
