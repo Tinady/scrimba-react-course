4571c88100717ff9892f87cedc3a94f65391db44
Tina Dessu <tinady2009@gmail.com>
2023-04-02 18:10:07 +0300

    Create nav.js

---
+++ airbnbclone/src/components/nav.js
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "../imgs/airbnb1.png"
+
+
+export default function Navbar(){
+   return(
+    <div className="navbar">
+        <img src={logo}/>
+    </div>
+   )
+
+}
