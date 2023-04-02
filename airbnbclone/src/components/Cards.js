f08c98659cfccde1d2dcc145fbb39493b38c982c
Tina Dessu <tinady2009@gmail.com>
2023-04-02 21:21:47 +0300

    cards

--- airbnbclone/src/components/Cards.js
+++ airbnbclone/src/components/Cards.js
@@ -1,10 +1,18 @@
 import React from "react";
+import photo from "../imgs/KatieZ.png"
 
 export default function Cards()
 {
     return(
-        <div>
-            <p>hey</p>
+        <div className="card-container">
+           <img className="cards-img" src={photo}/>
+           <div className="cards-sold"> 
+           <p > Sold out</p>
+           </div>
+          
+           <p>&bigstar 5.0 (6). USA</p>
+           <p>Life lessons with Katie Zaferes</p>
+           <p> From $316/person</p>
         </div>
     )
 }
