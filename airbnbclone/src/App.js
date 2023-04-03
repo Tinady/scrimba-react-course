f14c1344d28d747aeb0d1256237ecff76de9cfb9
Tina Dessu <tinady2009@gmail.com>
2023-04-03 18:15:57 +0300

    Update App.js

--- airbnbclone/src/App.js
+++ airbnbclone/src/App.js
@@ -2,14 +2,25 @@ import React from "react";
 import Navbar from "./components/nav"
 import Hero from "./components/Hero"
 import Cards from "./components/Cards"
+import Contacts from "./components/Contacts";
 import "./App.css"
 
 export default function App(){
    return(
     <div>
-        <Navbar/>
+        {
+        /* <Navbar/>
         <Hero/>
-        <Cards/>
+        <Cards/> */
+       
+          <div className="contact-cards">
+         <Contacts/>
+          <Contacts/>
+          <Contacts/>
+          <Contacts/>
+            </div>
+
+        }
     </div>
 
    )
