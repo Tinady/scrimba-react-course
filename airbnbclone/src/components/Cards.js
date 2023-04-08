e0a73d390accdf6b3be41a953fa1e2300289ee1c
Tina Dessu <tinady2009@gmail.com>
2023-04-08 12:14:36 +0300

    Update Cards.js

--- airbnbclone/src/components/Cards.js
+++ airbnbclone/src/components/Cards.js
@@ -2,19 +2,19 @@ import React from "react";
 import photo from "../imgs/KatieZ.png"
 import star from "../imgs/Star.png"
 
-export default function Cards()
+export default function Cards(props)
 {
     return(
         <div>
         <div className="card-container">
-           <img className="cards-img" src={photo}/>
+           <img className="cards-img" src={props.image}/>
            <div className="cards-sold"> 
            <p > SOLD OUT </p>
            </div>
           
-           <p> <img src={star} className="star"/> <span>5.0 (6). USA</span> </p>
-           <p>Life lessons with Katie Zaferes</p>
-           <p><b> From $316</b> <span>/person</span></p>
+           <p> <img src={star} className="star"/> <span> {props.rating} {props.review}. USA</span> </p>
+           <p>{props.title}</p>
+           <p><b> From {props.price}</b> <span>/person</span></p>
         </div>  
         </div>
     )
