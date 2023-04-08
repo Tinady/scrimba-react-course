74f14a21d368ff4df8a4bc9d74a09756305bac03
Tina Dessu <tinady2009@gmail.com>
2023-04-08 14:58:23 +0300

    Update data.js

--- airbnbclone/src/data.js
+++ airbnbclone/src/data.js
@@ -9,12 +9,12 @@ export default [
         title: "Life Lessons with Katie Zaferes",
         description: "I will share with you what I call \"Positively Impactful Moments of Disappointment.\" Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.",
         price: 136,
-        coverImg: {photo1},
+        coverImg: photo1,
         stats: {
             rating: 5.0,
-            reviewCount: 6
+            reviewCount: "(6)"
         },
-        location: "Online",
+        location: "USA",
         openSpots: 0,
     },
     {
@@ -22,12 +22,12 @@ export default [
         title: "Learn Wedding Photography",
         description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
         price: 125,
-        coverImg: {photo3},
+        coverImg: photo3,
         stats: {
             rating: 5.0,
-            reviewCount: 30
+            reviewCount:"(30)" 
         },
-        location: "Online",
+        location: "USA",
         openSpots: 27,
     },
     {
@@ -35,12 +35,12 @@ export default [
         title: "Group Mountain Biking",
         description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
         price: 50,
-        coverImg: {photo2},
+        coverImg: photo2,
         stats: {
             rating: 4.8,
-            reviewCount: 2
+            reviewCount:  "(2)"
         },
-        location: "Norway",
+        location: "USA",
         openSpots: 3,
     }
 ]
