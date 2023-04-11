1f738338b599826e835c00e5f5f13d8673b05abe
Tina Dessu <tinady2009@gmail.com>
2023-04-11 18:51:39 +0300

    Update App.js

--- memegenerator/src/App.js
+++ memegenerator/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
+import Meme from './components/meme';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
        <Header/>
+       <Meme/>
     </div>
   );
 }
