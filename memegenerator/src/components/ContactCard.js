086fc1ab568e0862f5531cef6c7fa9e3b6c89b00
Tina Dessu <tinady2009@gmail.com>
2023-04-17 14:54:09 +0300

    Update ContactCard.js

--- memegenerator/src/components/ContactCard.js
+++ memegenerator/src/components/ContactCard.js
@@ -1,9 +1,26 @@
 import React from "react";
+import user from  "../imgs/user.png"
+import filled from "../imgs/start-filled.jpg"
+import empty from "../imgs/start-empty.png"
 
 
 export default function ContactCard(){
+    const [contact, setContact] = React.useState({
+        firstName: "John",
+        lastName: "Doe",
+        phone: "+1 (719) 555-1212",
+        email: "itsmyrealname@example.com",
+        isFavorite: false
+    })
   return(
-    <div>
+    <div className="contact-container">
+    <img src={user} className="contact-img"/>
+    <img src={empty} className="contact-star"/>
+    <h3>{contact.firstName} {contact.lastName} </h3>
+    <p >{contact.phone} </p>
+    <p>{contact.email}  </p>
+
+
 
     </div>
   )
