86032995a8e968b7fdb71ee11fd059d8896f2b4c
Tina Dessu <tinady2009@gmail.com>
2023-04-17 15:51:14 +0300

    Update App.js

--- memegenerator/src/App.js
+++ memegenerator/src/App.js
@@ -8,12 +8,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-       {/* <Header/>
-       <Meme/> */
-       <ContactCard/>
-       }
-
-    </div>
+        <Header/>
+        <Meme/> 
+       </div>
   );
 }
 
