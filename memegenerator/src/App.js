d8e96515e68c63475f78799a919c2b91a8e123ec
Tina Dessu <tinady2009@gmail.com>
2023-04-17 14:53:57 +0300

    Update App.js

--- memegenerator/src/App.js
+++ memegenerator/src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import Header from './components/Header';
 import Meme from './components/meme';
+import ContactCard from './components/ContactCard';
+import './contact.css'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-       <Header/>
-       <Meme/>
+       {/* <Header/>
+       <Meme/> */
+       <ContactCard/>
+       }
+
     </div>
   );
 }
