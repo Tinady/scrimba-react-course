bb1718a875996eb3ceeb564e8f71638f6dc0c24b
Tina Dessu <tinady2009@gmail.com>
2023-04-19 17:57:04 +0300

    Create count.js

---
+++ memegenerator/src/components/count.js
@@ -0,0 +1,24 @@
+import React, {useState} from "react";
+
+export default  function Count(){
+    
+    const [count, setCount]=useState(0)
+     function Add(){
+        setCount(prevcount=>prevcount+1)
+     }
+
+     function substract(){
+        setCount(prevcount=>prevcount-1)
+     }
+    return(
+        <div className="count-container">
+               <button className="counter-minus" onClick={substract}>-</button>
+              <div className="count">
+              <h1>{count}</h1>
+              </div>
+              
+              <button className="counter-plus" onClick={Add}>+</button>
+
+        </div>
+    )
+}
