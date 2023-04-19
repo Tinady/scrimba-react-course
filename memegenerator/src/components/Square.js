6fe9647a85028bed804ec53a0e6750d0635e12d5
Tina Dessu <tinady2009@gmail.com>
2023-04-19 22:46:02 +0300

    Update Square.js

--- memegenerator/src/components/Square.js
+++ memegenerator/src/components/Square.js
@@ -1,12 +1,15 @@
 import React, {useState} from "react";
 import Boxes from "./Boxes";
 
-export default  function Square()
+export default  function Square(props)
 {
     const [squareElements, setSquareElements]=useState(Boxes)
+     const Style={
+        backgroundColor:props.darkMode?"black":"white"
+     }
 
     const squareDivs= squareElements.map(sq=>
-        {return <div className="squares" key={sq.id}></div>})
+        {return <div style={Style} className="squares" key={sq.id}></div>})
 
     return(
         <div>
