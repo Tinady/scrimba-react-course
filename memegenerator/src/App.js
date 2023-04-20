40e8c547334ed319692602ae6921d8d8fb22afbe
Tina Dessu <tinady2009@gmail.com>
2023-04-20 12:03:31 +0300

    Update App.js

--- memegenerator/src/App.js
+++ memegenerator/src/App.js
@@ -1,21 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Header from './components/Header';
 import Meme from './components/meme';
 import ContactCard from './components/ContactCard';
 import Count from './components/count';
 import Square from './components/Square';
+import Boxes from './components/Boxes';
 import './contact.css'
 import './App.css';
 
 function App() {
+
+  const [boxes, setBoxes]=useState(Boxes)
+
+  
+ 
+  const squareDivs=Boxes.map(sq=>
+      (<Square on={boxes.on} key={sq.id}/>))
   return (
     <div className="App">
         {/* <Header/>
         <Meme/>  
-        <Count/> */
-        <Square darkMode={false}/>
+        <Count/> */}
+        {squareDivs}
 
-        }
+        
         
        </div>
   );
