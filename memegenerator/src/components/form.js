e9184560120859e3c8f33e9cb8f86d9f84a5113a
Tina Dessu <tinady2009@gmail.com>
2023-04-21 19:06:51 +0300

    Update form.js

--- memegenerator/src/components/form.js
+++ memegenerator/src/components/form.js
@@ -1,11 +1,20 @@
 import React, {useState}from "react";
 
 export default function Forms(){
+
+    const [firstName, setFirstName]=useState('');
+    console.log(firstName)
+     function handleChange (event)
+     {
+        setFirstName(event.target.value)
+     }
+
   return (
   <form  className="form-container" >
     <input
      type="text"
      placeholder="First Name"
+     onChange={handleChange}
      />
 
 </form>)
