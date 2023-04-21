98115f9f6d595d9fe5b477f2510792974ce23379
Tina Dessu <tinady2009@gmail.com>
2023-04-21 16:53:11 +0300

    Update App.js

--- memegenerator/src/App.js
+++ memegenerator/src/App.js
@@ -5,36 +5,46 @@ import ContactCard from './components/ContactCard';
 import Count from './components/count';
 import Square from './components/Square';
 import Boxes from './components/Boxes';
+import jokeData from './components/jokeData';
+import Jokes from './components/joke';
 import './contact.css'
 import './App.css';
 
 export default function App() {
 
-  const [boxes, setBoxes]=useState(Boxes)
+  // const [boxes, setBoxes]=useState(Boxes)
   
-  function toggle(id){
-         setBoxes(prevstate=>
-          {return prevstate.map((square)=>{
-            return square.id==id?{...square, on:!square.on}:square;
-         })})
+  // function toggle(id){
+  //        setBoxes(prevstate=>
+  //         {return prevstate.map((square)=>{
+  //           return square.id==id?{...square, on:!square.on}:square;
+  //        })})
 
-    }
+  //   }
 
 
-  const squareDivs=boxes.map(sq=>
-      (<Square
-        key={sq.id}
-        on={sq.on} 
-        handleClick={()=>toggle(sq.id)}
-         />
-         ))
+  // const squareDivs=boxes.map(sq=>
+  //     (<Square
+  //       key={sq.id}
+  //       on={sq.on} 
+  //       handleClick={()=>toggle(sq.id)}
+  //        />
+  //        ))
+
+    const jokeelements= jokeData.map((j)=>
+    {return <Jokes
+         key={j.id}
+          setup={j.setup}
+          pinchline={j.punchline}
+             />})
          
   return (
     <div className="App">
         {/* <Header/>
         <Meme/>  
-        <Count/> */}
-        {squareDivs}
+        <Count/> 
+        {squareDivs} */}
+        {jokeelements}
 
         
         
