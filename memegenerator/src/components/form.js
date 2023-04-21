048b3d37764cc564f90f2be8ff5449c6779bbce2
Tina Dessu <tinady2009@gmail.com>
2023-04-21 18:48:43 +0300

    Create form.js

---
+++ memegenerator/src/components/form.js
@@ -0,0 +1,13 @@
+import React, {useState}from "react";
+
+export default function Forms(){
+  return (
+  <form  className="form-container" >
+    <input
+     type="text"
+     placeholder="First Name"
+     />
+
+</form>)
+    
+}
