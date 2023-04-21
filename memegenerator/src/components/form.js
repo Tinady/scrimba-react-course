d427f88d06c9837ab9ebb6f509751b80defb9676
Tina Dessu <tinady2009@gmail.com>
2023-04-21 20:46:53 +0300

    Update form.js

--- memegenerator/src/components/form.js
+++ memegenerator/src/components/form.js
@@ -2,11 +2,19 @@ import React, {useState}from "react";
 
 export default function Forms(){
 
-    const [firstName, setFirstName]=useState('');
-    console.log(firstName)
+     const [formData , setFormData]= useState({
+        firstName:'', 
+        lastName:'',
+        Email:''
+         })
+    console.log(formData)
+    
      function handleChange (event)
      {
-        setFirstName(event.target.value)
+        setFormData(prevState=>
+            {return {...prevState, 
+                      [event.target.name]:event.target.value}
+            })
      }
 
   return (
@@ -15,6 +23,24 @@ export default function Forms(){
      type="text"
      placeholder="First Name"
      onChange={handleChange}
+     name="firstName"
+     value={formData.firstName}
+     />
+      <input
+     type="text"
+     placeholder="Last Name"
+     onChange={handleChange}
+     name="lastName"
+     value={formData.lastName}
+     />
+
+<input
+     type="email"
+     placeholder="Email"
+     onChange={handleChange}
+     name="Email"
+     value={formData.Email}
+
      />
 
 </form>)
