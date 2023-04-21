e1a3de9082033654b2553896ccb320e857b3373a
Tina Dessu <tinady2009@gmail.com>
2023-04-21 17:07:11 +0300

    Update joke.js

--- memegenerator/src/components/joke.js
+++ memegenerator/src/components/joke.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 
 export default function Jokes(props){
+
+    const [isShown ,setIsShown]=useState(false)
+     function toggle(){
+        setIsShown(prevState=>!prevState)
+        console.log(isShown)
+
+     }
+     
+
     return(
+      
        <div>
-        
-         <p>{props.punchline}</p>
+         {props.setup && <h3>{props.setup}</h3>}
+         <p>{isShown && props.punchline}</p>
+         <button onClick={toggle}>Show</button>
          <hr/>
        </div>
     
