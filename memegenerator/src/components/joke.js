da33effe76659d63203d9734fea619a4fd9e2704
Tina Dessu <tinady2009@gmail.com>
2023-04-21 16:53:06 +0300

    Create joke.js

---
+++ memegenerator/src/components/joke.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Jokes(props){
+    return(
+       <div>
+        
+         <p>{props.punchline}</p>
+         <hr/>
+       </div>
+    
+    )
+}
