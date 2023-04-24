491564ee2fa3ae0a1a6244068f118f89cb82ad51
Tina Dessu <tinady2009@gmail.com>
2023-04-25 00:07:49 +0300

    Update Navbar.js

--- funfactsabtreact/src/components/Navbar.js
+++ funfactsabtreact/src/components/Navbar.js
@@ -14,7 +14,14 @@ function  Navbar(props){
        <img src={logo} className="nav--icon" />
             <h3 className="nav--logo_text">ReactFacts</h3>
             <h4 className="nav--title">React Course - Project 1</h4>
-      
+            <input type="checkbox"
+                   onClick={props.handleClick}
+                   id="checkBox"
+                   className="toggle"
+                   />
+          <label htmlFor="checkBox"></label>
+          
+
       </nav>
     )
 
