998348f5e5ce713d897a6cf185121f02425a7a04
Tina Dessu <tinady2009@gmail.com>
2023-04-24 19:54:00 +0300

    Update windowTracker.js

--- memegenerator/src/components/windowTracker.js
+++ memegenerator/src/components/windowTracker.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, {useState, useEffect}from "react";
 
 export default function WindowTracker()
 {
+     const [windowWidth, setWindowWidth]=useState(window.innerWidth)
+        useEffect(()=>
+          {window.addEventListener("resize", ()=>
+            {setWindowWidth(window.innerWidth)})},[]
+          )
+
     return(
         <div>
           <button>Toggle Window Tracker</button>
-          <p>Window size: {window.innerWidth}</p>
+          <p>Window size: {windowWidth}</p>
 
 
         </div>
