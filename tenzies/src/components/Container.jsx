42661b4220d02470421b306e8859d57d095f4d5f
Tina Dessu <tinady2009@gmail.com>
2023-04-30 22:22:45 +0300

    Update Container.jsx

--- tenzies/src/components/Container.jsx
+++ tenzies/src/components/Container.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import Die from "./Die";
 import "./Container.css"
 
 export default function Container(){
+    const nums= [1,2,3,4,5,6,7]
+    
+
+    const D= nums.map((num)=>
+       {return <Die key={num} value={num}/>
+      }
+       )
+
        return(
         <div className="container">
-
-
-        </div>
+         {D}
+       </div>
        )
 }
