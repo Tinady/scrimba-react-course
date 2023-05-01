3b430e2afcb134c7ff6ddafab476a35ab88007df
Tina Dessu <tinady2009@gmail.com>
2023-05-01 21:07:09 +0300

    Create Container.jsx

---
+++ tenzies/src/components/Container/Container.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Die from "../Dice/Die";
+import "./Container.css"
+
+export default function Container(){
+    const nums= [1,2,3,4,5,6,7,8,9,10]
+    
+
+    const D= nums.map((num)=>
+       {return <Die key={num} value={num}/>
+      }
+       )
+
+       return(
+        <div className="container">
+         {D}
+       </div>
+       )
+}
