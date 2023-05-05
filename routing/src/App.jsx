d33537473a444f1490ab6ec531facd5cee53ec97
Tina Dessu <tinady2009@gmail.com>
2023-05-05 21:52:38 +0300

    fixing

--- routing/src/App.jsx
+++ routing/src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './components/Home'
 import './App.css'
 
 function App() {
   
 
   return (
+    <Routes>
+      <Route  path="/" element={<Home/>}/>
+    </Routes>
     
-      <div>Hello World </div>
        
   )
 }
