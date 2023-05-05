20d78d89e5574965627bd21ed8c9e8c2762bdd8e
Tina Dessu <tinady2009@gmail.com>
2023-05-05 22:05:23 +0300

    random

--- routing/src/App.jsx
+++ routing/src/App.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Link, Route, Routes } from 'react-router-dom'
 import Home from './components/Home'
+import Book from './components/Book'
 import './App.css'
 
 function App() {
   
 
   return (
+    <>
+    <nav>
+      <ul>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/books'>Books</Link></li>
+      </ul>
+    </nav>
     <Routes>
       <Route  path="/" element={<Home/>}/>
+      <Route path='/books'  element={<Book/>}/>
     </Routes>
     
-       
+    </>    
   )
 }
 
