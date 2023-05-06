03975647f3fde02b81375ea27a07e4e4a3d5dc9f
Tina Dessu <tinady2009@gmail.com>
2023-05-06 12:45:25 +0300

    Update BookList.jsx

--- routing/src/components/BookList.jsx
+++ routing/src/components/BookList.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Book(){
  return(
+   <>
     <h1>BookList</h1>
+    <Link to='/books/1'>Book 1</Link>
+    <br/>
+    <Link to='/books/2'>Book 2 </Link>
+    </>
  )
 }
