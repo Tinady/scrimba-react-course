8121895010f3942cdc8a97254eb0b22e5b1e1e8c
Tina Dessu <tinady2009@gmail.com>
2023-05-06 14:20:38 +0300

    Create BookLayout.jsx

---
+++ routing/src/components/BookLayout.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function BookLayout(){
+    return(
+        <>
+        <Link to='/books/1'>Book 1</Link>
+        <br/>
+        <Link to='/books/2'>Book 2 </Link>
+        <br/>
+        <Link to='/books/new'>New Books</Link>
+        </>
+        )
+}
