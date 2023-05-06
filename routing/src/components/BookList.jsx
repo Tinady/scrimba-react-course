7b426b5fbd0de3e92bd954dfb7cb916ba07f3e5f
Tina Dessu <tinady2009@gmail.com>
2023-05-06 14:20:28 +0300

    Update BookList.jsx

--- routing/src/components/BookList.jsx
+++ routing/src/components/BookList.jsx
@@ -5,11 +5,7 @@ export default function Book(){
  return(
    <>
     <h1>BookList</h1>
-    <Link to='/books/1'>Book 1</Link>
-    <br/>
-    <Link to='/books/2'>Book 2 </Link>
-    <br/>
-    <Link to='/books/new'>New Books</Link>
+   
     </>
  )
 }
