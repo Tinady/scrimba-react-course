95737616219aa6725914fe3f0b9acab2e6cd987b
Tina Dessu <tinady2009@gmail.com>
2023-05-07 10:59:47 +0300

    Update Book.jsx

--- routing/src/components/Book.jsx
+++ routing/src/components/Book.jsx
@@ -3,9 +3,9 @@ import { useOutletContext, useParams } from "react-router-dom";
 
 export default function Book(){
    const {id}=useParams()
-   const obj=useOutletContext()
+   
  return(
  
-    <h1>Book {id} {obj.hello}</h1>
+    <h1>Book {id} </h1>
  )
 }
