c5ac60f0cf1ddce668f679d98ca378e31184d850
Tina Dessu <tinady2009@gmail.com>
2023-05-07 11:38:00 +0300

    Update BookLayout.jsx

--- routing/src/components/BookLayout.jsx
+++ routing/src/components/BookLayout.jsx
@@ -4,9 +4,9 @@ import { Link, Outlet } from 'react-router-dom';
 export default function BookLayout(){
     return(
         <>
-        <Link to='/books/1'>Book 1</Link>
+        <Link to='/books/1' replace>Book 1</Link>
         <br/>
-        <Link to='/books/2'>Book 2 </Link>
+        <Link to='/books/2' replace>Book 2 </Link>
         <br/>
         <Link to='/books/new'>New Books</Link>
         <Outlet />
