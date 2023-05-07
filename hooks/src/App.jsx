afad32cdb336d9f36990e448aaefa654b462d77d
Tina Dessu <tinady2009@gmail.com>
2023-05-07 11:55:08 +0300

    Update App.jsx

--- hooks/src/App.jsx
+++ hooks/src/App.jsx
@@ -3,11 +3,15 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [resources, SetResources]=useState('Posts')
 
   return (
     <>
-   
+     <button onClick={()=>SetResources('Posts')}>Posts</button>
+     <button onClick={()=>SetResources('Users')}>Users</button>
+     <button onClick={()=>SetResources('Comments')}>Comments</button>
+
+     <h1>{resources}</h1>
       
     </>
   )
