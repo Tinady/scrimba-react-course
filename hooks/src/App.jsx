b5524e64dfa9fb4cae1ac86f1b69936f80ef2a09
Tina Dessu <tinady2009@gmail.com>
2023-05-07 17:35:12 +0300

    Update App.jsx

--- hooks/src/App.jsx
+++ hooks/src/App.jsx
@@ -3,27 +3,20 @@ import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [resources, SetResources]=useState('Posts')
-  const [item, setItem]=useState([])
-
+  
+  const [windowWidth, setWindowWidth]=useState(window.innerWidth)
+  
+  function HandleResize(){
+    setWindowWidth(window.innerWidth)
+  }
+  
   useEffect(()=>
-  {fetch(`https://jsonplaceholder.typicode.com/${resources}`)
-  .then(response => response.json())
-  .then(json => setItem(json))
-},
-   [resources])
-
+  {window.addEventListener('resize',HandleResize),
+   [windowWidth]})
+  
   return (
     <>
-     <button onClick={()=>SetResources('posts')}>Posts</button>
-     <button onClick={()=>SetResources('users')}>Users</button>
-     <button onClick={()=>SetResources('comments')}>Comments</button>
-
-     <h1>{resources}</h1>
-     {item.map(item => {
-      {return<pre>{JSON.stringify(item)}</pre>}
-     })}
-  
+     {windowWidth}
       
     </>
   )
