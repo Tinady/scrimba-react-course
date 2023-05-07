27a95b9ab9d7e43b9c1958b696d0edcd9182373b
Tina Dessu <tinady2009@gmail.com>
2023-05-07 12:48:06 +0300

    usEffect example 1

--- hooks/src/App.jsx
+++ hooks/src/App.jsx
@@ -1,17 +1,29 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import './App.css'
 
 function App() {
   const [resources, SetResources]=useState('Posts')
+  const [item, setItem]=useState([])
+
+  useEffect(()=>
+  {fetch(`https://jsonplaceholder.typicode.com/${resources}`)
+  .then(response => response.json())
+  .then(json => setItem(json))
+},
+   [resources])
 
   return (
     <>
-     <button onClick={()=>SetResources('Posts')}>Posts</button>
-     <button onClick={()=>SetResources('Users')}>Users</button>
-     <button onClick={()=>SetResources('Comments')}>Comments</button>
+     <button onClick={()=>SetResources('posts')}>Posts</button>
+     <button onClick={()=>SetResources('users')}>Users</button>
+     <button onClick={()=>SetResources('comments')}>Comments</button>
 
      <h1>{resources}</h1>
+     {item.map(item => {
+      {return<pre>{JSON.stringify(item)}</pre>}
+     })}
+  
       
     </>
   )
